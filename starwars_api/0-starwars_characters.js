77424231effccbba8c3d22649acce94e061da6fe
Ho2bes <8696@holbertonstudents.com>
2025-04-18 09:04:29 +0200

    fix outpout

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -4,35 +4,32 @@ const request = require('request');
 
 const movieId = process.argv[2];
 if (!movieId) {
-  console.error('Usage: ./0-starwars_characters.js <movie_id>');
+  console.error('Usage: ./0-starwars_characters.js <Movie ID>');
   process.exit(1);
 }
 
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
-request(url, (err, res, body) => {
-  if (err) {
-    console.error(err);
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
     return;
   }
 
-  const data = JSON.parse(body);
-  const characters = data.characters;
+  const filmData = JSON.parse(body);
+  const characters = filmData.characters;
 
-  const printCharacter = (index) => {
-    if (index >= characters.length) return;
-
-    request(characters[index], (err2, res2, body2) => {
-      if (err2) {
-        console.error(err2);
-        return;
-      }
-
-      const characterData = JSON.parse(body2);
-      console.log(characterData.name);
-      printCharacter(index + 1);
+  const getCharacter = (url) =>
+    new Promise((resolve, reject) => {
+      request(url, (err, res, body) => {
+        if (err) reject(err);
+        else resolve(JSON.parse(body).name);
+      });
     });
-  };
 
-  printCharacter(0);
+  Promise.all(characters.map(getCharacter))
+    .then((names) => {
+      names.forEach((name) => console.log(name));
+    })
+    .catch((err) => console.error(err));
 });
